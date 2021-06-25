2d17d45bd9ae2b41e0736336cd498d0bcc73a8f5
T-ZhenLS <zh3nl5.20@gmail.com>
2021-06-25 13:31:16 +0800

    ColorTween and AnimationController

--- lib/shared/heart.dart
+++ lib/shared/heart.dart
@@ -5,7 +5,37 @@ class Heart extends StatefulWidget {
   _HeartState createState() => _HeartState();
 }
 
-class _HeartState extends State<Heart> {
+class _HeartState extends State<Heart> with SingleTickerProviderStateMixin {
+  AnimationController _animationController;
+  Animation _colorAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    /**
+     * Create controller to handle animation
+     * duration: Duration of animation
+     * vsync: Sync the animation with the ticker
+     */
+    _animationController =
+        AnimationController(duration: Duration(milliseconds: 200), vsync: this);
+
+    /**
+     * Use ColorTween for Color transition
+     * Use .animate() to bind ColorTween with an animation controller
+     */
+    _colorAnimation = ColorTween(begin: Colors.grey[400], end: Colors.redAccent)
+        .animate(_animationController);
+
+    _animationController.forward();
+
+    _animationController.addListener(() {
+      print(_animationController.value);
+      print(_colorAnimation.value);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return IconButton(
