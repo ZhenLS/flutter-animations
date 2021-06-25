904c47ba0ae8e9dc8245e259b9dcb2c5ce78e2a0
T-ZhenLS <zh3nl5.20@gmail.com>
2021-06-25 16:54:05 +0800

    Stagger and SlideTransition for List

--- lib/shared/tripList.dart
+++ lib/shared/tripList.dart
@@ -9,46 +9,63 @@ class TripList extends StatefulWidget {
 
 class _TripListState extends State<TripList> {
   List<Widget> _tripTiles = [];
-  final GlobalKey _listKey = GlobalKey();
+  final GlobalKey<AnimatedListState> _listKey = GlobalKey<AnimatedListState>();
 
   @override
   void initState() {
     super.initState();
-    _addTrips();
+    // Only fire the function after build() has run
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _addTrips();
+    });
   }
 
   void _addTrips() {
     // get data from db
     List<Trip> _trips = [
-      Trip(title: 'Beach Paradise', price: '350', nights: '3', img: 'beach.png'),
+      Trip(
+          title: 'Beach Paradise', price: '350', nights: '3', img: 'beach.png'),
       Trip(title: 'City Break', price: '400', nights: '5', img: 'city.png'),
       Trip(title: 'Ski Adventure', price: '750', nights: '2', img: 'ski.png'),
       Trip(title: 'Space Blast', price: '600', nights: '4', img: 'space.png'),
     ];
 
+    Future future = Future(() {});
+
     _trips.forEach((Trip trip) {
-      _tripTiles.add(_buildTile(trip));
+      future = future.then((_) {
+        return Future.delayed(const Duration(milliseconds: 200), () {
+          _tripTiles.add(_buildTile(trip));
+          _listKey.currentState.insertItem(_tripTiles.length - 1);
+        });
+      });
     });
   }
 
   Widget _buildTile(Trip trip) {
     return ListTile(
       onTap: () {
-        Navigator.push(context, MaterialPageRoute(builder: (context) => Details(trip: trip)));
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => Details(trip: trip)));
       },
       contentPadding: EdgeInsets.all(25),
       title: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           Text('${trip.nights} nights',
-              style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold, color: Colors.blue[300])),
-          Text(trip.title, style: TextStyle(fontSize: 20, color: Colors.grey[600])),
+              style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blue[300])),
+          Text(trip.title,
+              style: TextStyle(fontSize: 20, color: Colors.grey[600])),
         ],
       ),
       leading: ClipRRect(
         borderRadius: BorderRadius.circular(8.0),
         child: Hero(
-          tag: "location-img-${trip.img}", // Unique Key for animate images // has to be same with image in connected pages
+          tag: "location-img-${trip.img}",
+          // Unique Key for animate images // has to be same with image in connected pages
           child: Image.asset(
             'images/${trip.img}',
             height: 50.0,
@@ -59,14 +76,18 @@ class _TripListState extends State<TripList> {
     );
   }
 
+  Tween<Offset> _offset = Tween(begin: Offset(2, 0), end: Offset(0, 0));
+
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      key: _listKey,
-      itemCount: _tripTiles.length,
-      itemBuilder: (context, index) {
-        return _tripTiles[index];
-      }
-    );
+    return AnimatedList(
+        key: _listKey,
+        initialItemCount: _tripTiles.length,
+        itemBuilder: (context, index, animation) {
+          return SlideTransition(
+            child: _tripTiles[index],
+            position: animation.drive(_offset),
+          );
+        });
   }
 }
